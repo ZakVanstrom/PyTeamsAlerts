7c9d3ade8185fc3da3b1eb3aab345fdbdd76b85f
Zak Vanstrom <zvanstro@asu.edu>
2020-06-30 15:41:31 -0400

    accept string or list for basic alert

--- lib/TeamsAlert.py
+++ lib/TeamsAlert.py
@@ -1,6 +1,15 @@
 import pymsteams
 
 def sendAlert(hooks, title, text):
+    if isinstance(hooks, str):
+        try:
+            message = pymsteams.connectorcard(hooks)
+            message.title(title)
+            message.text(text)
+            message.send()
+        except:
+            print("The message failed to send to the Webhook at " + hooks)
+        return
     for x in hooks:
         try:
             message = pymsteams.connectorcard(x)
