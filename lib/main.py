5fb1251ebd684d27c9c0b74f7f9c7bc82cf7c936
Zak Vanstrom <zvanstro@asu.edu>
2020-07-10 11:52:58 -0400

    small testing updates

--- lib/main.py
+++ lib/main.py
@@ -1,7 +1,7 @@
-import TeamsAlert
-
+from TeamsAlert import sendAlert
+#
 # teamsMessage.sendFromDirectory("hooks.txt", "Test Message -> Read from file, from a main file. Test 2 with Error Handling")
-
+#
 # teamsMessage.addHookToDirectory("hooks.txt", "this is a line")
 #
 # newDB = ToyDB.ToyDB("webhook_storage.json")
@@ -14,4 +14,6 @@ import TeamsAlert
 # for x in newDB.db:
 #     print(newDB.db[x])
 
-TeamsAlert.sendAlert(["https://outlook.office.com/webhook/d262c1e6-4556-40c1-a740-9d4154da9b3e@7b65f535-32b5-4d73-9282-1c761c779561/IncomingWebhook/70c931703e024464a7fe048c486f6e6c/7f2e5c23-c925-47a1-9d8c-644326c2a1d4"], "test", "test text")
+
+a = ["https://outlook.office.com/webhook/d262c1e6-4556-40c1-a740-9d4154da9b3e@7b65f535-32b5-4d73-9282-1c761c779561/IncomingWebhook/70c931703e024464a7fe048c486f6e6c/7f2e5c23-c925-47a1-9d8c-644326c2a1d4", "https://outlook.office.com/webhook/d262c1e6-4556-40c1-a740-9d4154da9b3e@7b65f535-32b5-4d73-9282-1c761c779561/IncomingWebhook/70c931703e024464a7fe048c486f6e6c/7f2e5c23-c925-47a1-9d8c-644326c2a1d4", "https://outlook.office.com/webhook/d262c1e6-4556-40c1-a740-9d4154da9b3e@7b65f535-32b5-4d73-9282-1c761c779561/IncomingWebhook/70c931703e024464a7fe048c486f6e6c/7f2e5c23-c925-47a1-9d8c-644326c2a1d4"]
+sendAlert(a, "test", "test text")
